e4bee2af7aa188d11f0f5fd39ebbd25590259522
Dmitry <agapov.box@gmail.com>
2020-09-20 23:09:45 +0300

    Add role admin for the first registered user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true
+  before_create :set_admin
+
+  private def set_admin
+    self.admin = true unless User.exists?
+  end
+
 end
