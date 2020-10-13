3f4ee96753255ad48ba244a4c13594e25093236a
Dmitry <agapov.box@gmail.com>
2020-10-13 20:19:50 +0300

    Users don't see other users order. Save order session for login user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
+      session[:order_id] = @user.id
       redirect_to '/profile'
     else
       redirect_to '/login'
