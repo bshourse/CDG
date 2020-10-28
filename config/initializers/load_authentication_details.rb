f5083589e9c9510ec8b8df90060d55ceb0770ec1
Dmitry <agapov.box@gmail.com>
2020-10-28 15:00:56 +0300

    Add new conf2 test

--- config/initializers/load_authentication_details.rb
+++ config/initializers/load_authentication_details.rb
@@ -1,1 +1,1 @@
-AUTH_DETAILS = YAML.load_file("#{Rails.env = "development"}/config/authentication_details.yml")
+AUTH_DETAILS = YAML.load_file("#{Rails.env = "test"}/config/authentication_details.yml")
